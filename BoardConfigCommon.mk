fdb9c4e147c0add3f4061d0de886226219a80ddf
xNombre <kartapolska@gmail.com>
2021-07-01 11:33:48 +0200

    sm6250-common: Disable serial console
    Nobody uses it on production.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: Idb8d5ceb7eb7b0fe59fc080f55ab3f119fd5b3fc
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -92,12 +92,10 @@ BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
 
-BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem,nosocket
-BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8
 BOARD_KERNEL_CMDLINE += earlycon=msm_geni_serial,0xa88000
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
