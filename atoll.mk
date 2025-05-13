3784408f23be38559ddc59b3fe06ef6d998830f8
SirRGB <sirrgb@proton.me>
2025-05-13 23:26:25 +0200

    sm6250-common: overlay: set our own ota url
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- atoll.mk
+++ atoll.mk
@@ -268,7 +268,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SM6250Aperture \
     SM6250LineageDialer \
-    SM6250LineageSDK
+    SM6250LineageSDK \
+    SM6250Updater
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
